b392a1b2834545bacfe3113c88feb80cbfa8c5bc
Zeljko Markovic <zeljko.markovic@hotmail.ch>
2025-02-27 18:53:49 +0100

    Add initial Spring Boot module for APIs

---
+++ user-api/src/test/java/box/pandora/user_api/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package box.pandora.user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
