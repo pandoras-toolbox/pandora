f5c6c6b54fffac82ce480fdcf56c68ac1708f710
Zeljko Markovic <zeljko.markovic@hotmail.ch>
2025-02-28 08:37:18 +0100

    Add REST services to renamed Spring Boot application

---
+++ payroll/src/main/java/box/pandora/payroll/EmployeeNotFoundAdvice.java
@@ -0,0 +1,17 @@
+package box.pandora.payroll;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+class EmployeeNotFoundAdvice {
+
+	@ExceptionHandler(EmployeeNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String employeeNotFoundHandler(EmployeeNotFoundException e) {
+		return e.getMessage();
+	}
+
+}
---
+++ payroll/src/main/java/box/pandora/payroll/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package box.pandora.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
